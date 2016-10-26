873857d85f67655ae9c292db7eb762281d63cec0
Qi Li <hkustliqi@gmail.com>
2016-10-26 10:12:23 -0700

    skipped already asked case number

--- tracker.py
+++ tracker.py
@@ -11,11 +11,7 @@ br = Browser()
 myCaseNum = 1690654088
 date = 'May 12'
 formType = 'Form I-485'
-numRange = 1
-numTotalCase = 0
-numApproved = 0
-numRejected = 0
-numFPReceived = 0
+numRange = 400
 dataBase = {}
 
 # Ignore robots.txt
@@ -24,45 +20,56 @@ br.set_handle_robots( False )
 br.addheaders = [('User-agent', 'Firefox')]
 
 with open('data.txt') as infile:
-  data = json.load(infile)
-print data
+  dataBase = json.load(infile)
 
 # query USCIS check my case webpage
 for n in range (0-numRange, numRange):
-  # Retrieve USCIS website
-  br.open( "http://egov.uscis.gov/casestatus/landing.do" )
-  # Select the form
-  br.select_form( 'caseStatusForm' )
-  # print br.form
-  caseNum = 'LIN' + str(myCaseNum + n)
-  br.form["appReceiptNum"] = caseNum
+  caseNum = str(myCaseNum + n)
+  if caseNum not in dataBase:
+    # Retrieve USCIS website
+    br.open( "http://egov.uscis.gov/casestatus/landing.do" )
+    # Select the form
+    br.select_form( 'caseStatusForm' )
+    # print br.form
+    br.form["appReceiptNum"] = 'LIN' + caseNum
 
-  # Get the response
-  br.submit()
-  html = br.response().read()
-  soup = BeautifulSoup(html, 'html.parser')
-  # print soup.prettify()
+    # Get the response
+    br.submit()
+    html = br.response().read()
+    soup = BeautifulSoup(html, 'html.parser')
+    # print soup.prettify()
 
-  # get current case status
-  for status in soup.findAll('div', {'class': 'rows text-center'}):
-    if all (keyWord in status.text for keyWord in [formType, date]):
-      print(status.text)
-      receiptNum = re.search('LIN(\d+)', status.text).group(1)
-      print receiptNum
-      numTotalCase += 1
-      if 'Fingerprint Fee Was Received' in status.text:
-        numFPReceived += 1
-        dataBase[receiptNum] = 'Fingerprint Fee Was Received'
-      elif 'Case Was Approved' in status.text:
-        numApproved += 1
-        dataBase[receiptNum] = 'Case Was Approved'
-      elif 'Case Rejected' in status.text:
-        numRejected += 1
-        dataBase[receiptNum] = 'Case Rejected'
+    # get current case status
+    for status in soup.findAll('div', {'class': 'rows text-center'}):
+      if all (keyWord in status.text for keyWord in [formType, date]):
+        print(status.text)
+        receiptNum = re.search('LIN(\d+)', status.text).group(1)
+        if 'Fingerprint Fee Was Received' in status.text:
+          dataBase[receiptNum] = 'Fingerprint Fee Was Received'
+        elif 'Case Was Approved' in status.text:
+          dataBase[receiptNum] = 'Case Was Approved'
+        elif 'Case Rejected' in status.text:
+          dataBase[receiptNum] = 'Case Rejected'
+
+
+numTotalCase = 0
+numApproved = 0
+numRejected = 0
+numFPReceived = 0
+
+for case in dataBase:
+  numTotalCase += 1
+  if dataBase[case]=='Fingerprint Fee Was Received':
+    numFPReceived += 1
+  elif dataBase[case]=='Case Was Approved':
+    numApproved += 1
+  elif dataBase[case]=='Case Rejected':
+    numRejected += 1
 
 # store data
 with open('data.txt', 'w') as outfile:
   json.dump(dataBase, outfile)
+
 
 # Print final statistics
 print '*********************************'
