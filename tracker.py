380767f358da9dd571a07c25ed1108bfb600ec8b
mikehawke1337 <misc@sowas.net>
2020-06-30 18:33:23 -0500

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -8,10 +8,15 @@ from mechanize import Browser
 import os.path
 br = Browser()
 
-
-myCaseNum = 1690654088
+# Enter Service Center code below MSC = National Benefits Center
+mySC = 'MSC'
+# Enter Case Number without Service Center
+myCaseNum = 1234567890
+# Form Type to look for
 formType = 'Form I-485'
-numRange = 400
+# Range either direction of your Case Number
+numRange = 10
+
 dataBase = {}
 visited = {}
 
@@ -33,11 +38,12 @@ for n in range (0-numRange, numRange):
   if caseNum not in visited:
     visited[caseNum] = 'visited'
     # Retrieve USCIS website
-    br.open( "http://egov.uscis.gov/casestatus/landing.do" )
+    # URL seems to have changed
+    br.open( "https://egov.uscis.gov/casestatus/mycasestatus.do" )
     # Select the form
     br.select_form( 'caseStatusForm' )
     # print br.form
-    br.form["appReceiptNum"] = 'LIN' + caseNum
+    br.form["appReceiptNum"] = mySC + caseNum
 
     # Get the response
     br.submit()
@@ -49,38 +55,50 @@ for n in range (0-numRange, numRange):
     for status in soup.findAll('div', {'class': 'rows text-center'}):
       if all (keyWord in status.text for keyWord in [formType]):
         print(status.text)
-        receiptNum = re.search('LIN(\d+)', status.text).group(1)
+        receiptNum = re.search(mySC + '(\d+)', status.text).group(1)
         if 'Fingerprint Fee Was Received' in status.text:
           dataBase[receiptNum] = 'Fingerprint Fee Was Received'
+		  # Added fingerprints taken as separate line to show progress
+        elif 'Case Was Updated To Show Fingerprints Were Taken' in status.text:
+          dataBase[receiptNum] = 'Fingerprints Were Taken'
         elif 'Case Was Approved' in status.text:
           dataBase[receiptNum] = 'Case Was Approved'
-        elif any (deny in status.text for deny in ['Case Was Rejected', 'Decision Notice Mailed']):
+		  # Capitalization of the word was has been changed apparently
+        elif any (deny in status.text for deny in ['Case Was Rejected', 'Decision Notice Mailed', 'Case Rejected']):
           dataBase[receiptNum] = 'Case Rejected'
         elif 'Case Was Received' in status.text:
           dataBase[receiptNum] = 'Case Received'
-        elif 'Case Is Ready To Be Scheduled For An Interview' in status.text:
+        elif 'Case is Ready to Be Scheduled for An Interview' in status.text:
           dataBase[receiptNum] = 'Ready for Interview'
-        elif any (RFE in status.text for RFE in ['Request for Additional Evidence Was Mailed', 'Request For Evidence Was Received']):
+		  # Added RfIE
+        elif any (RFE in status.text for RFE in ['Request for Additional Evidence Was Mailed', 'Request For Evidence Was Received', 'Request for Initial Evidence Was Sent']):
           dataBase[receiptNum] = 'RFE'
         elif 'Case Was Transferred' in status.text:
           dataBase[receiptNum] = 'Case Transferred'
         elif 'Name Was Updated' in status.text:
           dataBase[receiptNum] = 'Name Updated'
+		  # Added Card Mailed
+        elif 'Card Was Mailed To Me' in status.text:
+          dataBase[receiptNum] = 'Card Mailed'		  
 
 numTotalCase = 0
 numApproved = 0
 numRejected = 0
 numFPReceived = 0
+numCaseWithFP = 0
 numReceived = 0
 numInterview = 0
 numRFE = 0
 numTransfer = 0
 numNameUpdated = 0
+numCardMailed = 0
 
 for case in dataBase:
   numTotalCase += 1
   if dataBase[case]=='Fingerprint Fee Was Received':
     numFPReceived += 1
+  elif dataBase[case]=='Fingerprints Were Taken':
+    numCaseWithFP += 1
   elif dataBase[case]=='Case Was Approved':
     numApproved += 1
   elif dataBase[case]=='Case Rejected':
@@ -95,6 +113,8 @@ for case in dataBase:
     numTransfer += 1
   elif dataBase[case]=='Name Updated':
     numNameUpdated += 1
+  elif dataBase[case]=='Card Mailed':
+    numCardMailed += 1
 
 # store data
 with open('data.txt', 'w') as outfile:
@@ -104,14 +124,23 @@ with open('visited.txt', 'w') as outfile:
 
 template = '{0:45}{1:5}'
 # Print final statistics
-print '*********************************'
-print 'For ' + str(2*numRange) + ' neighbors of LIN' + str(myCaseNum) +', we found the following statistics: '
-print template.format('total number of I-485 application received: ', str(numTotalCase))
-print template.format('Case Was Approved: ', str(numApproved))
-print template.format('Fingerprint Fee Was Received: ', str(numFPReceived))
-print template.format('Case Was Rejected: ', str(numRejected))
-print template.format('Case Was Received: ', str(numReceived))
-print template.format('Case Was Ready for Interview: ', str(numInterview))
-print template.format('Case is RFE: ', str(numRFE))
-print template.format('Case Was Transferred: ', str(numTransfer))
-print template.format('Name Was Updated: ', str(numNameUpdated))
+print ('*********************************')
+print ('For ' + str(2*numRange) + ' neighbors of ' + mySC + str(myCaseNum) +', we found the following statistics: ')
+print (template.format('total number of I-485 application received: ', str(numTotalCase)))
+
+print ('\nPositives:')
+print (template.format('Case Was Approved: ', str(numApproved)))
+print (template.format('Card Was Mailed: ', str(numCardMailed)))
+print (template.format('Case Is Ready for Interview: ', str(numInterview)))
+
+print ('\nNegatives:')
+print (template.format('Case is RFE: ', str(numRFE)))
+print (template.format('Case Was Rejected: ', str(numRejected)))
+
+print ('\nNeutral:')
+print (template.format('Case Was Received: ', str(numReceived)))
+print (template.format('Fingerprint Fee Was Received: ', str(numFPReceived)))
+print (template.format('Fingerprints Were Taken: ', str(numCaseWithFP)))
+print (template.format('Case Was Transferred: ', str(numTransfer)))
+print (template.format('Name Was Updated: ', str(numNameUpdated)))
+
